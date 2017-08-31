1380041d01190a120bfedaf16f99ca144dd54d10
scott elson <aguyawry@users.noreply.github.com>
2017-08-30 20:54:27 -0400

    contact and tools

--- dev/index.php
+++ dev/index.php
@@ -84,7 +84,7 @@
             <div class="col-6">
                 <p>Everything in the signal chain matters.</p>
                 <p>Some tools add color while others are transparent, so we choose gear wisely - only using what’s needed and appropriate.</p>
-                <p>We have both analog and digital processing availablle depending on what feels right for the music.</p>
+                <p>We have processing available in both the analog and digital domains depending on what feels right for the music, and quality conversion to go between them.</p>
             </div>
 
             <div class="col-group">
@@ -118,37 +118,40 @@
 
         <div class="col-group">
             <div class="col-6">
-                <p>Please get in touch well before you are ready for mastering to ensure we can meet your timeline. As a supporter of the independent music community, we offer competitive rates for independent projects.</p>
-
-                <p>Giving back to the community is also important to us and we enjoy being able to offer volunteer services when possible. Please call, email or fill out our quote form and we’ll find a way to work within your budget.</p>
-
-                <p>Join us on Facebook and other social media channels for regular updates from the studio.</p>
+                <p>Please get in touch well before you are ready for mastering to ensure we can meet your timeline. Please call, email or fill out our <a href="https://docs.google.com/forms/d/e/1FAIpQLScSzOwOeRaMF7oyx1yHC2eohTOBkC4MroBaiv_wCR0baFPTew/viewform" target="_blank">inquiry form</a> to start the booking process.</p>
+                <p>As a supporter of the independent music community, we offer competitive rates for independent projects. We'll do our best to work with your budget.</p>
+                <p>Giving back to the community is important to us and we enjoy offering volunteer services when possible.</p>
+                <p>Join us on Facebook and other social media channels for regular updates from the studio. Send us your latest news and we'll pass it on.</p>
             </div>
 
             <div class="col-5 col-offset-1">
-                <p><a class="an-contact-phone" href="tel:207.358.9146" title="Call elson mastering at 207.358.9146">207.358.9146</a></p>
-                <p><script type="text/javascript">
-                    //<![CDATA[
-                    <!--
-                    var x="function f(x){var i,o=\"\",l=x.length;for(i=l-1;i>=0;i--) {try{o+=x.c" +
-                    "harAt(i);}catch(e){}}return o;}f(\")\\\"function f(x,y){var i,o=\\\"\\\\\\\""+
-                    "\\\\,l=x.length;for(i=0;i<l;i++){y%=127;o+=String.fromCharCode(x.charCodeAt" +
-                    "(i)^(y++));}return o;}f(\\\"\\\\<69.180+N\\\\\\\\026\\\\\\\\020\\\\\\\\n\\\\"+
-                    "\\\\020\\\\\\\\000\\\\\\\\n\\\\\\\\t@KV\\\\\\\\nL\\\\\\\\016\\\\\\\\002\\\\" +
-                    "\\\\016\\\\\\\\003\\\\\\\\002O/V\\\\\\\\024\\\\\\\\030Z\\\\\\\\033\\\\\\\\0" +
-                    "26\\\\\\\\024\\\\\\\\017\\\\\\\\035\\\\\\\\036\\\\\\\\n-dobmiZ%(axnj0R-}p{\\"+
-                    "\\\\\\177`z,dll~rs]{sSNLNEVRBZ@DL\\\\\\\\002NABl\\\\\\\\023\\\\\\\\022G]AZR" +
-                    "\\\\\\\\005e\\\\\\\\030^Q\\\\\\\\\\\\\\\\WS`$.0++f*):>.>$ (\\\\\\\\014sl   " +
-                    "2>7\\\\\\\\031?7/202\\\\\\\\001\\\\\\\\022\\\\\\\\026\\\\\\\\006\\\\\\\\026" +
-                    "\\\\\\\\014\\\\\\\\010\\\\\\\\000F\\\\\\\\n\\\\\\\\005\\\\\\\\006PB\\\\\\\\" +
-                    "017QRXICO\\\"\\\\,88)\\\"(f};)lo,0(rtsbus.o nruter};)i(tArahc.x=+o{)--i;0=>" +
-                    "i;1-l=i(rof}}{)e(hctac};l=+l;x=+x{yrt{)84=!)31/l(tAedoCrahc.x(elihw;lo=l,ht" +
-                    "gnel.x=lo,\\\"\\\"=o,i rav{)x(f noitcnuf\")"                                 ;
-                    while(x=eval(x));
-                    //-->
-                    //]]>
-                    </script>
+                <p>
+                    <a class="btn" href="https://docs.google.com/forms/d/e/1FAIpQLScSzOwOeRaMF7oyx1yHC2eohTOBkC4MroBaiv_wCR0baFPTew/viewform" target="_blank">project inquiries</a>
                 </p>
+
+                <div class="hcard">
+                    <p>
+                        <a class="p-tel" href="tel:2073589146" title="Call elson mastering at 207.358.9146">207.358.9146</a>
+                        <script type="text/javascript">
+                        //<![CDATA[
+                        <!--
+                        var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+                        "=55){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
+                        ".substr(0,ol);}f(\")911,\\\"QYSND[500\\\\L^410\\\\710\\\\<p:22(<,$78:<!=571" +
+                        "0\\\\!$(>>:ve230\\\\(+ l&.VLXHH[TVXEYQs]XTZZ^620\\\\D^EAFK700\\\\x630\\\\DD" +
+                        "Rw>?@wsxur;`6O/bcnbn,j6+ ij`pjpv.n\\\\320\\\\130\\\\620\\\\710\\\\230\\\\72" +
+                        "0\\\\320\\\\\\\"(f};o nruter};))++y(^)i(tAedoCrahc.x(edoCrahCmorf.gnirtS=+o" +
+                        ";721=%y{)++i;l<i;0=i(rof;htgnel.x=l,\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"     ;
+                        while(x=eval(x));
+                        //-->
+                        //]]>
+                        </script>
+                    </p>
+                </div>
+                
+
+
+            
             </div>
             
         </div>
