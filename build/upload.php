9844bde34c3d6f92054160099237ec2742a24245
scott elson <aguyawry@users.noreply.github.com>
2018-04-25 21:40:54 -0400

    remove old files

--- build/upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $page = "upload";
-    $title = "upload audio | elson mastering";
-    $description = "elson mastering provides high-quality audio mastering services to artists, labels and music producers throughout the world.";
-    $keywords = "scott elson, mastering, audio mastering, music, audio encoding, music production, audio restoration, transfers, archives, forensic audio, maine";
-    include "top.php";
-?><section id="intro" class="light"><div class="container"><div class="col-group"><div class="col-mb-12 col-6"><h2>Your music is your story.</h2><h2>We’re all ears.</h2><h3>Superior mastering.</h3><h3>For anyone.</h3><h3>Anywhere.</h3></div><div class="col-mb-12 col-6"><p>elson mastering is dedicated to making high quality audio mastering accessible to all artists regardless of where they are in their career.</p><p>Mastering engineer Scott Elson's goal is to make each client’s artistic vision a sonic reality by bringing out the absolute best in musicality and sound quality.</p><p>He collaborates with artists, producers, engineers and labels from all parts of the world, and from a wide range of musical genres.</p><p>Get in touch to find out how we can help with your next project or send us a track for a free mix evaluation.</p><div class="btn-row center"><p> <a class="btn" href="https://docs.google.com/forms/d/e/1FAIpQLScSzOwOeRaMF7oyx1yHC2eohTOBkC4MroBaiv_wCR0baFPTew/viewform" rel="external">Get a Quote</a> <a class="btn" href="" rel="external">Upload a Track</a></p></div></div></div></div></section> <?php
-    include "bottom.php";
-?>
