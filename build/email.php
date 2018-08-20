a3d3b2aa818cec9e3e9d588beed8912cee263515
scott elson <aguyawry@users.noreply.github.com>
2018-08-20 13:44:49 -0400

    new build

---
+++ build/email.php
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+//<![CDATA[
+<!--
+var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+"=55){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
+".substr(0,ol);}f(\")911,\\\"QYSND[500\\\\L^410\\\\710\\\\<p:22(<,$78:<!=571" +
+"0\\\\!$(>>:ve230\\\\(+ l&.VLXHH[TVXEYQs]XTZZ^620\\\\D^EAFK700\\\\x630\\\\DD" +
+"Rw>?@wsxur;`6O/bcnbn,j6+ ij`pjpv.n\\\\320\\\\130\\\\620\\\\710\\\\230\\\\72" +
+"0\\\\320\\\\\\\"(f};o nruter};))++y(^)i(tAedoCrahc.x(edoCrahCmorf.gnirtS=+o" +
+";721=%y{)++i;l<i;0=i(rof;htgnel.x=l,\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"     ;
+while(x=eval(x));
+//-->
+//]]>
+</script>
