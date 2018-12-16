06072f51ef14ba18fb9cf7c68d1fd3bbf0574592
scott elson <aguyawry@users.noreply.github.com>
2018-12-15 23:50:17 -0500

    initial rates page

---
+++ dev/rates.php
@@ -0,0 +1,136 @@
+<?php
+    $page = "rates";
+    $title = "rates | elson mastering";
+    $description = "elson mastering rates and services ";
+    $keywords = "scott elson, music mastering, audio mastering, mastered for iTunes, remote mastering, internet mastering, music production, audio restoration, transfers, archives, maine";
+    $robots = "noindex, nofollow";
+    include "top.php";
+?>
+
+<section class="dark">
+    <div class="container">
+        <div class="col-group">
+
+            <div class="col-mb-12">
+                <h2>rates</h2>
+
+                <p>Please find rates for services below. I realize every project is unique and I will do my best to work with your budget, so don't hesitate to reach out.<p>
+
+                <h3>Mastering</h3>
+
+                <p>All mastering rates include:</p>
+            </div>
+
+            <div class="col-mb-12 col-6">
+
+                <ul class="list-bullet">
+                    <li><strong>Mix Assessments</strong><br>The best place to start in mastering is with a good mix. If you are unsure about your mixes, I am happy to give them a listen and provide feedback on potential enhancements.</li>
+
+                    <li><strong>File Transfers</strong><br>You can easily send your mixes to me by uploading them via my DropBox account. I'll deliver all mastered audio the same way.</li>
+
+                    <li><strong>Session Set Up</strong><br>The time it takes to set up my gear and prepare your audio for the mastering session.</li>
+
+                    <li><strong>High Resolution Files</strong><br>All mastering is done at high resolution. Any other file formats you need will be created from these.</li>
+                </ul>
+            </div>
+
+            <div class="col-mb-12 col-6">
+                <ul class="list-bullet">
+                    <li><strong>Sample Rate / Bit Depth Conversions</strong><br>It's possible you'll need different file formats for various purposes - like 44.1 kHz / 16 bit files for streaming services or 48 kHz / 24 bit for video.</li>
+
+                    <li><strong>Two Rounds of Revisions</strong><br>Most projects don't require revisions, but I'll include up to 2 revisions per track. Further revisions are billed hourly at $150/hr.</li>
+
+                    <li><strong>Archives</strong><br>While I highly recommend making mutliple copies of your mastered audio and keeping them in different places, I'll keep your audio archived for two years just in case.</li>
+
+                    <li><strong>Communications</strong><br>Email correspondence, video chat, phone calls...</li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="col-group">
+            
+            <div class="col-mb-12">
+                <h4>Unattended Mastering</h4>
+            </div>
+
+            <div class="col-mb-12 col-6">    
+                <p>Unattended mastering is billed per audio minute, with a 5 audio minute minimum.</p>
+                <p><strong>Example 1</strong><br>
+                New client, 30 minutes of audio, standard mastering:<br>
+                30 minutes x $15 per audio minute = $450
+                </p>
+
+                <p><strong>Example 2</strong><br>
+                Returning client, 3 minutes of audio, stem mastering<br>
+                5 minutes (minimum) x $18 per audio minute = $90
+                </p>
+            </div>
+
+            <div class="col-mb-12 col-6">
+                <table>
+                    <tr>
+                        <td colspan='2'><strong>Standard Mastering</strong></td>
+                    </tr>
+                    <tr>
+                        <td>&bull; New Clients</td>
+                        <td>$15</td>
+                    </tr>
+                    <tr>
+                        <td>&bull; Returning Clients</td>
+                        <td>$13</td>
+                    </tr>
+                    <tr>
+                        <td>&bull; TV / Instrumentals / Radio Edits</td>
+                        <td>$4</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td colspan='2'><strong>Stem Mastering</strong></td>
+                    </tr>
+                    <tr>
+                        <td>&bull; New Clients</td>
+                        <td>$20</td>
+                    </tr>
+                    <tr>
+                        <td>&bull; Returning Clients</td>
+                        <td>$18</td>
+                    </tr>
+                    <tr>
+                        <td>&bull; TV / Instrumentals / Radio Edits</td>
+                        <td>$6</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-group">    
+            <div class="col-mb-12">
+                <p><small><sup>1</sup> The Client is the person or entity who is paying the bill. This could be the artist, producer, label or other.</small></p>
+            </div>
+        </div>
+
+        <div class="col-group">    
+            <div class="col-mb-12">
+                <h4>Attended Mastering</h4>
+                <p>Attended mastering sessions are billed hourly at $150/hr.</p>
+            </div>
+        </div>
+
+        <div class="col-group">    
+            <div class="col-mb-12">
+                <h4>Mastering Calculator</h4>
+                <p></p>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+
+<?php
+    include "bottom.php";
+?> 
