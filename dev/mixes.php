c2fa69b4c01e1b30e1d81defe92d29f86c180625
scott elson <aguyawry@users.noreply.github.com>
2020-03-27 22:02:07 -0400

    Update mixes.php

--- dev/mixes.php
+++ dev/mixes.php
@@ -19,7 +19,7 @@
                 <p>I highly encourage you to read this entire article, but if you're looking for a quick summary, here you go!</p>
 
                 <ul class="list-bullet">
-                    <li>Send 24 bit files that are the same sample rate as your mix project.</li>
+                    <li>Send 24 bit WAV or AIFF files that are the <strong><em>same sample rate as your mix project</em></strong>. Please don't sample rate convert your files or send MP3s.</li>
                 </ul>
             </div>
         </div>
@@ -42,14 +42,19 @@
                 <h4>Sample Rate</h4>
                 <p>Your choice of sample rate will usually be determined by where your audio is ultimatly going to be used. At a minimum, you'll want to record at 44.1 kHz which is the standard for the Compact Disc, but as the CD becomes obsolete and streaming services now dominate the world, you should consider a minimum of 48 kHz instead.</p>
 
-                <p>Another reason to consider 48 kHz is because it's the de facto standard for pretty much all video projects. If you're thinking of trying to get your music placed in television or movies (and you should!), 48 kHz is the way to go. A number of organizations such as <a href="https://www.grammy.com/recording-academy/membership/recording-academy/about/chapters/producers-engineers-wing">The Recording Academy Producers & Engineers Wing</a> and <a href="https://www.riaa.com/" rel="external">The Recording Industry Association of America (RIAA)</a> have also defined "high resolution" audio as <em>recordings which have been mastered from better than CD quality (48 kHz/20-bit or higher) music sources</em>.</p>
+                <p>Another reason to consider 48 kHz is because it's the de facto standard for pretty much all video projects. If you're thinking of trying to get your music placed in television or movies (and you should!), 48 kHz is the way to go.</p>
 
-                <p>You can certainly record at higher sample rates like 96 kHz or higher.</p>
+                <p>A number of organizations such as <a href="https://www.grammy.com/recording-academy/membership/recording-academy/about/chapters/producers-engineers-wing">The Recording Academy Producers & Engineers Wing</a> and <a href="https://www.riaa.com/" rel="external">The Recording Industry Association of America (RIAA)</a> have also actually defined "high resolution" audio as <em>recordings which have been mastered from better than CD quality (48 kHz/20-bit or higher) music sources</em>.</p>
+
+                <p>You can certainly record at much higher sample rates such as 96 kHz or more. I encourage you to experiment by listening to see if you personally hear the difference. In the early 2000's, there was a trend to record music at higher sample rates due to the format wars between <a href="https://en.wikipedia.org/wiki/DVD-Audio" rel="external">DVD-Audio</a> and the <a href="https://en.wikipedia.org/wiki/Super_Audio_CD" rel="external">Super Audio CD (SACD)</a> which was encoded via <a href="https://en.wikipedia.org/wiki/Direct_Stream_Digital" rel="external">Direct Stream Digital (DSD)</a>, not via <a href="https://en.wikipedia.org/wiki/Pulse-code_modulation" rel="external">Pulse-Code Modulation (PCM)</a>.</p>
+
+                <p>Since then, consumers' tastes have trended towards downloadable music vs physical discs. For the typical musician, a 48 kHz sample rate will provide an ample dynamic range and signal to noise ratio. If you are going for more of an audiophile approach, by all means consider a higher sample rate.
 
                 <h4>Bit Depth</h4>
+                <p>mastering enigneer with D/A through an analog chain </p>
 
                 <h4>What To Send To Mastering</h4>
-                <p>The short answer is, send 24 bit files <strong><em>at the same sample sample rate as your mixes. Do not sample rate convert your files</em></strong>. Upsampling files doesn't make them sound better and downsampling will likley make them sound worse.</p>
+                <p>The short answer is, send 24 bit WAV or AIFF files <strong><em>at the same sample sample rate as your mixes. Do not sample rate convert your files</em></strong>. Upsampling files doesn't make them sound better and downsampling will likley make them sound worse.</p>
 
                 <p>Also note that higher sample rates and bit depths will increase file sizes, and will tax your audio workstation more if that is a concern. While hard disk space is realtively inexpensive, a 48 kHz / 24 bit file will typically provide excellent sound quality at a nominal file size, while also positioning you well for a potential marraige with video. 96 kHz / 24 bit is another great choice.</p>
 
