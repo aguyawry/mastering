587bf2f00d116537e7d19b84fa8d4e6b9edd2422
scott elson <aguyawry@users.noreply.github.com>
2020-04-01 22:27:10 -0400

    Update mixes.php

--- dev/mixes.php
+++ dev/mixes.php
@@ -16,10 +16,14 @@
 
                 <p>Clients often ask what kind of files they should send for mastering. - focus on digital, not analog, DAT, or non-PCM like DSD which a mojority of music creators will be working </p>
 
+                <p>Assumes you are familiar with the basic concepts of digital audio. Alwasy feel free to reach out with any questions or clarifications and I am happy to help.</p>
+
+                <p>You will definitely find lots of debate about many of these topics. In the end, some of it is personal preference and convienince, while some is audio best practices to make sure your music sounds the best it can.</p>
+
                 <p>I highly encourage you to read this entire article, but if you're looking for a quick summary, here you go!</p>
 
                 <ul class="list-bullet">
-                    <li>Send 24 bit WAV or AIFF files that are the <strong><em>same sample rate as your mix project</em></strong>. Please don't sample rate convert your files or send MP3s.</li>
+                    <li>Send 24 bit WAV or AIFF files at the <strong><em>same sample rate as your mix project</em></strong>. Please don't sample rate convert your files or send MP3s.</li>
                 </ul>
             </div>
         </div>
@@ -33,42 +37,50 @@
 
         <div class="col-group">
             <div class="col-mb-12">
-                <h3>Mixes Really Start With Tracking</h3>
 
-                <p>As you begin the actual recording process and are creating a session for tracking in your workstation, you'll be faced with a couple of important decisions - choosing a sample rate and bit depth. There are a number of options for each and a reasons why you'd want to choose one over the other.</p>
+                <h3>Sample Rates and Bit Depth</h3>
+
+                <h4>Mastering Really Start With Tracking</h4>
+
+                <p>As the digital audio worksation has become a nearly ubiquitous tool in music production, it's important to realize the choices you make when starting the recording process will ripple through to mixing and mastering.</p>
+
+                 <p>Two of the more important decisions you'll make is choosing a sample rate and bit depth. There are a number of options for each and various reasons why you'd want to choose one over the other.</p>
 
                 <p></p>
 
                 <h4>Sample Rate</h4>
-                <p>Your choice of sample rate will usually be determined by where your audio is ultimatly going to be used. At a minimum, you'll want to record at 44.1 kHz which is the standard for the Compact Disc, but as the CD becomes obsolete and streaming services now dominate the world, you should consider a minimum of 48 kHz instead.</p>
+                <p>Your choice of sample rate will usually be determined by where your audio is ultimatly going to be used. At a minimum, you'll want to record at 44.1 kHz which is the standard for the Compact Disc (CD), but as the CD becomes obsolete and digital delivery now dominates the world, you should strongly consider a minimum of 48 kHz instead.</p>
 
-                <p>Another reason to consider 48 kHz is because it's the de facto standard for pretty much all video projects. If you're thinking of trying to get your music placed in television or movies (and you should!), 48 kHz is the way to go.</p>
+                <p>Another reason to choose 48 kHz is because it's the de facto standard for most video projects. If you're thinking of trying to get your music placed in television or movies (and you should!), 48 kHz is the way to go.</p>
 
-                <p>A number of organizations such as <a href="https://www.grammy.com/recording-academy/membership/recording-academy/about/chapters/producers-engineers-wing">The Recording Academy Producers & Engineers Wing</a> and <a href="https://www.riaa.com/" rel="external">The Recording Industry Association of America (RIAA)</a> have also actually defined "high resolution" audio as <em>recordings which have been mastered from better than CD quality (48 kHz/20-bit or higher) music sources</em>.</p>
+                <p>A number of organizations such as <a href="https://www.grammy.com/recording-academy/membership/recording-academy/about/chapters/producers-engineers-wing">The Recording Academy Producers & Engineers Wing</a> and <a href="https://www.riaa.com/" rel="external">The Recording Industry Association of America (RIAA)</a> have also <a href="https://www.riaa.com/high-resolution-audio-initiative-gets-major-boost-with-new-hi-res-music-logo-and-branding-materials-for-digital-retailers/" rel="external">agreed to define high resolution audio</a> as "<em>recordings which have been mastered from better than CD quality (48 kHz/20-bit or higher) music sources</em>."</p>
 
-                <p>You can certainly record at much higher sample rates such as 96 kHz or more. I encourage you to experiment by listening to see if you personally hear the difference. In the early 2000's, there was a trend to record music at higher sample rates due to the format wars between <a href="https://en.wikipedia.org/wiki/DVD-Audio" rel="external">DVD-Audio</a> and the <a href="https://en.wikipedia.org/wiki/Super_Audio_CD" rel="external">Super Audio CD (SACD)</a> which was encoded via <a href="https://en.wikipedia.org/wiki/Direct_Stream_Digital" rel="external">Direct Stream Digital (DSD)</a>, not via <a href="https://en.wikipedia.org/wiki/Pulse-code_modulation" rel="external">Pulse-Code Modulation (PCM)</a>.</p>
+                <p>You can certainly record at much higher sample rates such as 96 kHz or more. The are a few techinal reasons why this may provide some advantages, but I encourage you to experiment and actually listen to see if you personally hear the difference.</p>
 
-                <p>Since then, consumers' tastes have trended towards downloadable music vs physical discs. For the typical musician, a 48 kHz sample rate will provide an ample dynamic range and signal to noise ratio. If you are going for more of an audiophile approach, by all means consider a higher sample rate.
+                <p>For the typical musician, a 48 kHz sample rate will sound great. If you are going for more of an audiophile approach or are targeting an esoteric delivery format with a specification that supports higher sample rates, go with what makes sense for you.
+
 
                 <h4>Bit Depth</h4>
-                <p>mastering enigneer with D/A through an analog chain </p>
+
+                <p>For many of the same reasons above, choosing a 24 bit file format to send to mastering (vs 16 bit) is going to be your ideal option and will provide an ample dynamic range and signal to noise ratio. If you're also targeting your release for <a href="https://www.apple.com/itunes/mastered-for-itunes/" rel="external">Apple Digital Masters</a>, your mixes must be at least 24 bit.</p>
+
+                <p>Another choice your workstation may allow is a 32 bit floting point file. This bit depth allows for storage of a much wider range of values and also has some advantages </p>
+
+                <p>Pretty much every digital audio workstation does its internal math at 32 bit or more likley 64 bit resolution, and will then reduce that down to whatever bit depth you have choosen for file creation and storage. Plugs-in may not be able to operate with the same precision</p>
+
+                <p>Keep in mind when you are listening to your music, there is a Digital to Analog (DAC) converter at the end of your chain that is more than likely operating at 24 bit resolution. There are many DACs out there that now operate at 32 bit resolution (integer, not floating point!), but if you look at their specfifications for dynamic range, they are still under the 144 dB that 24 bits offer.</p>
+
+                 <p>mastering enigneer with D/A through an analog chain </p>
 
                 <h4>What To Send To Mastering</h4>
-                <p>The short answer is, send 24 bit WAV or AIFF files <strong><em>at the same sample sample rate as your mixes. Do not sample rate convert your files</em></strong>. Upsampling files doesn't make them sound better and downsampling will likley make them sound worse.</p>
+                
+                <p>The short answer is send 24 bit WAV or AIFF files <strong><em>at the same sample sample rate as your mixes. Do not sample rate convert your files</em></strong>. Upsampling files doesn't make them sound better and downsampling will likley make them sound worse.</p>
 
-                <p>Also note that higher sample rates and bit depths will increase file sizes, and will tax your audio workstation more if that is a concern. While hard disk space is realtively inexpensive, a 48 kHz / 24 bit file will typically provide excellent sound quality at a nominal file size, while also positioning you well for a potential marraige with video. 96 kHz / 24 bit is another great choice.</p>
+                <p>Also note that higher sample rates and bit depths will increase file size, and will tax your audio workstation more if that is a concern. You might also find that some of your plugins may not work at the highest sample rates.</p>
 
-                <p>The goal throughout the music production process is to keep your audio at the highest quality possible. Once mastering is complete, your mastering engineer can create high quality sample rate and bit depth conversions as needed. They can also create high quality encodes to formats such as MP3 or AAC.</p>
+                <p>While hard disk space is realtively inexpensive, a 48 kHz / 24 bit file will typically provide excellent sound quality at a nominal file size, while also positioning you well for a potential marraige with video. 96 kHz / 24 bit is another great choice.</p>
 
-                    
-
-
-                    Always record at 24 bit resolution. Video 48k. Always best to start at a higher resolution and bit depth and convert down. Upsampling doesn’t create any more detail. Used to target formats like DVD-Audio, or even DVD-Video, Blu-ray optional Dolby True HD 192 kHz, DTS-HD 96kHz
-
-Higher sample rates capture more sonic information. Whether or not you or your fans will hear a difference is unknown. There are those that argue they “feel” a difference and that the frequencies we can’t hear still have an effect on our physical bodies in some way.
-
-                </p>
-
+                <p>The goal throughout the music production process is to keep your audio at the highest quality possible. Once mastering is complete, your mastering engineer can create any files you need with great sounding sample rate and bit depth conversions. They can also create high quality encodes to lossy formats such as MP3 or AAC.</p>
             </div>
         </div>
 
